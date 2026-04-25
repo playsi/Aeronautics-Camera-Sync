c86f88bdf70f7bc7c1da3087a93df28f4efe738b
playsi <efilonenko57@gmail.com>
2026-04-25 16:53:35 +0300

    - client config - now full client mod

--- src/main/java/com/playsi/aero_cam_sync/AeroCamSync.java
+++ src/main/java/com/playsi/aero_cam_sync/AeroCamSync.java
@@ -3,15 +3,9 @@ package com.playsi.aero_cam_sync;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.ModContainer;
 
 @Mod(AeroCamSync.MODID)
 public class AeroCamSync {
     public static final String MODID = "aero_cam_sync";
     public static final Logger LOGGER = LogUtils.getLogger();
-
-    public AeroCamSync(ModContainer modContainer) {
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
 }
--- src/main/java/com/playsi/aero_cam_sync/client/AeroCamSyncClient.java
+++ src/main/java/com/playsi/aero_cam_sync/client/AeroCamSyncClient.java
@@ -1,11 +1,13 @@
 package com.playsi.aero_cam_sync.client;
 
 import com.playsi.aero_cam_sync.AeroCamSync;
+import com.playsi.aero_cam_sync.Config;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
@@ -16,6 +18,7 @@ import static com.playsi.aero_cam_sync.AeroCamSync.MODID;
 @EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
 public class AeroCamSyncClient {
     public AeroCamSyncClient(ModContainer container) {
+        container.registerConfig(ModConfig.Type.CLIENT, Config.SPEC);
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
