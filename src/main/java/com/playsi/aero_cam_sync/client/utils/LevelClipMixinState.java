1b24a0b2ea9ed151089b9e7d94adf82accab38f2
playsi <efilonenko57@gmail.com>
2026-05-25 02:41:09 +0300

    - fix double pick

--- src/main/java/com/playsi/aero_cam_sync/client/utils/LevelClipMixinState.java
+++ src/main/java/com/playsi/aero_cam_sync/client/utils/LevelClipMixinState.java
@@ -1,5 +1,8 @@
 package com.playsi.aero_cam_sync.client.utils;
 
+import net.minecraft.world.level.ClipContext;
+
 public class LevelClipMixinState {
     public static boolean inTiltedClip = false;
+    public static ClipContext tiltedContext = null;
 }
